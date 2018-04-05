ff945f90007306bd9c55afe0494de6089235f238
Christopher Coté <Christopher Cot>
2018-04-05 11:43:42 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Lifx.Mixfile do
 
   def project do
     [app: :lifx,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
